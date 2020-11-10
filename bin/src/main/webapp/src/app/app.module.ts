a792c37e40433f8a63a690947c948cc8ad352c2a
chemabr24 <JoseManuel.Bastante@alu.uclm.es>
2020-11-10 08:43:56 +0100

    bin

---
+++ bin/src/main/webapp/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ReunionesComponent } from './reuniones/reuniones.component';
+import { ReunionesPreComponent } from './reuniones-pre/reuniones-pre.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { MenuComponent } from './menu/menu.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ReunionesComponent,
+    ReunionesPreComponent,
+    MenuComponent,
+    LoginComponent
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ bin/src/main/webapp/src/app/login/login.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
+  error: string;
+  success: string;
+  username: string;
+  password: string;
+  constructor(
+      private formBuilder: FormBuilder,
+      private route: ActivatedRoute,
+      private router: Router,
+      private UsuarioService: AuthService
+      
+  ){
+
+  }
+  ngOnInit(): void { 
+    this.loginForm = this.formBuilder.group({
+      usuario: ['', Validators.required],
+      password: ['', Validators.required]
+  });
+  }
+  // getter para obtener los controles del form
+  get f() { 
+    return this.loginForm.controls;
+  }
+
+
+ public login(){
+    this.submitted = true;
+    console.log("nombre"+this.username)
+    this.UsuarioService.login(this.username, this.password)
+    .subscribe(
+        data => {
+           this.router.navigate(['/reuniones']);
+        },
+            
+        error => {
+            this.error = "Error: No se ha encontrado ningún usuario con esos datos.";
+            
+        });
+  }
